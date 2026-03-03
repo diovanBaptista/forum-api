133d774644940da13eed83acea5c58d1b88dca36
Diovan <diovantrab@gmail.com>
2026-03-02 21:36:25 -0300

    feat: create signin auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { Prisma, User } from 'generated/prisma/browser';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
+
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    @Inject()
+    private readonly userService: UserService;
+
+    async signin(params: Prisma.UserCreateInput): Promise<Omit<User,'password'>> {
+        const user = await this.userService.user({email: params.email});
+        if (!user) throw new NotFoundException('User not found');
+        const passworMatch = await bcrypt.compare(params.password, user.password);
+        if (!passworMatch) throw new UnauthorizedException('invalid credentials');
+
+        const {password, ...result} = user;
+
+        return result
+    }
+
+
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -6,6 +6,7 @@ import { DatabaseModule } from 'src/database/database.module';
 @Module({
   imports: [DatabaseModule], // 👈 ISSO É OBRIGATÓRIO
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
